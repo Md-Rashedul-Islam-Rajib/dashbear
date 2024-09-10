3130d09c8d63382dcb41ef0581f219367730b85e
Md Rashedul Islam <rajib5570@gmail.com>
2024-09-10 22:02:00 +0600

    database connected successfully

--- server/schema/schema.ts
+++ server/schema/schema.ts
@@ -1,7 +1,7 @@
-import { serial } from "drizzle-orm/mysql-core";
-import { pgTable, text } from "drizzle-orm/pg-core";
+
+import { pgTable, serial, varchar } from "drizzle-orm/pg-core";
 
 export const posts = pgTable('post', {
     id : serial('id').primaryKey().notNull(),
-    title : text('title').notNull(),
+    title : varchar('title', {length: 255}).notNull(),
 })
